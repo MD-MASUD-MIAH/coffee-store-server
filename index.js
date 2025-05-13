8b5a197f5b23cace38606c7ab4d1f806e6fcf924
176409 <148116817+176409@users.noreply.github.com>
2025-05-13 21:52:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express') 
+const cors = require('cors')
+const app = express() 
+
+const port = process.env.PORT || 4000 
+
+// midleserer  
+
+app.use(cors()) 
+app.use(express.json())
+
+
+app.get('/',(req,res)=>{
+
+
+
+    res.send('coffee server is running')
+})
+
+app.listen(port,()=>{
+
+
+    console.log(`my port is  running ${port}`);
+    
+})
